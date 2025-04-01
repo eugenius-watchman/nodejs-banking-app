2ce483591884365867c09ccb7af56a276515af5d
eugenius-watchman <eugenius4real@gmail.com>
2025-04-01 23:18:40 +0000

    DB operations Fix negative balance withdrawals and improve account validation in banking app" [main d0b5a12] DB operations Fix negative balance withdrawals and improve account validation in banking app

---
+++ userinput.js
@@ -0,0 +1,10 @@
+// const user_input = (msg) =>
+//   new Promise((resolve, reject) => {
+//     rl.question(`\n ${msg}`, option => {
+//       resolve(option);
+//     });
+//   });
+
+//   const enterAccountName = async () => {
+    
+//   }
